98e6b0fcef0f77de9de019887a97cbe51f282978
Rodrigo Paczkovski <paczweb@gmail.com>
2020-08-16 16:50:58 -0300

    documentação: envio do sistema interativo de vendas utilizando readline

---
+++ back-end/aula-15/readme.md
@@ -0,0 +1,20 @@
+# Exercícios - Aula 15
+## Sistema interativo via readline
+
+Um sistema interativo de uma venda utilizando o módulo readline e chalk, simulando um cliente pedindo um produto querendo buscar um produto para comprá-lo.
+
+### Incialização do sistema
+```bashed
+node script.js
+```
+
+### Comandos disponívies
+O comando 
+``` sacola ```
+lista os produtos guardados na sacola, 
+``` procurar ```
+busca pelos produtos pelo através do nome,
+``` sair ```
+finaliza o sistema.
+
+Produtos disponíveis: ``skol``, ``kaiser``, ``itaipava`` e ``stella``.
--- back-end/aula-15/script.js
+++ back-end/aula-15/script.js
@@ -1,28 +1,175 @@
+const chalk = require('chalk')
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+//produtos disponíveis: skol, kaiser, itaipava e stella
 
-const products = {
-  []
+
+const products = [
+  {
+    id: 1,
+    name: "Skol",
+    amount: 100,
+    price: 400
+  },
+  {
+    id: 2,
+    name: "Kaiser",
+    amount: 50,
+    price: 350
+  },
+  {
+    id: 3,
+    name: "Itaipava",
+    amount: 25,
+    price: 300
+  },
+  {
+    id: 3,
+    name: "Stella",
+    amount: 25,
+    price: 600
+  }
+]
+
+const myWallet = []
+
+const seachProduct = (products, name) => {
+  let flag = false
+  let index = null
+  for(let i = 0; i < products.length; i++){
+    const product = products[i]
+    if(product.name.toUpperCase() === name.toUpperCase()){
+      flag = true
+      index = i    
+    }
+  }
+
+  if(!flag){
+    console.log(`Não temos o produto ${chalk.red(name)}`)
+  } else {
+    console.log(`Yay! Temos seu produto ${chalk.green(name)}!`)
+    availabilityProduct(products[index])
+  }
+
+  init()
 }
 
+const toTransformPriceInReal = (price) => {
+  return (price/100).toFixed(2)
+}
 
+const addWallet = (newProduct) => {
 
+  let flag = false
+  if(myWallet.length > 0){
+  
+    for(let i = 0; i < myWallet.length; i++){
+        if(myWallet[i].id === newProduct.id){
+            myWallet[i].amount +=  newProduct.amount
+            flag = true
+            break
+        } 
+    }
+  }
+  if(!flag) {
+    myWallet.push(newProduct)
+  }
+  upadeAmountProducts(newProduct)
+  console.log(`${chalk.green("Produto adicionado com sucesso a sacola!")}`)
+  init()
+}
 
+const listWallet = () => {
+  if(myWallet.length > 0){
+    console.log(myWallet)
+  } else {
+    console.log(`Sua sacola está vázia!`)
+  }
+  init()
+}
 
+const upadeAmountProducts = (newProduct) => {
+  for(let i = 0; i < products.length; i++){
+    if(products[i].id === newProduct.id){
+      products[i].amount -=  newProduct.amount
+      break
+    } 
+  }
+}
 
+// const payment = () => {
+//   rl.question('Deseja fazer o pagamento ou procurar outro produto? Digite: sim ou procurar? ', (answer) => {
+//     if(answer === 'sim' || answer === 'Sim') {
+//       console.log(`${chalk.yellow("Obrigado! Por favor se dirija ao caixa.")}`)
+//       rl.close();
+//     } else if (answer === 'procurar' || answer === 'Procurar'){
+//       serachProductQuestion()
+//     }
+//   })
+// }
 
+const availabilityProduct = (productInfo) => {
+  let limit = productInfo.amount
+  let price = productInfo.price
 
-const perguntarAlgo = () => {
-    rl.question('O que é algo? ', (resposta) => {
-        console.log(resposta)
+  rl.question(`Quantos produtos deseja comprar? Mínimo 1 e máximo ${limit}: `, (answer) => {
+    // if(answer = Number.isInteger(answer)) 
+    if(answer <= limit && answer >= 1){
+      
+      let newProduct = {
+          id: productInfo.id,
+          name: productInfo.name,
+          amount: parseInt(answer),
+          price: productInfo.price
+        }
+     
+      console.log(`Existe a quantidade de ${chalk.green(answer)} no estoque e cada unidade custa R$ ${chalk.green(toTransformPriceInReal(price))}`)  
+      addWalletQuestion(newProduct)
+
+    } else if (answer > limit) {
+      console.log(`A quantidade de ${chalk.red(answer + " é maior que a disponível")}  no estoque`)
+    } else {
+      console.log(`${chalk.red("Erro! Valor ou comando inválido!")}`)
+      rl.close();
+    }
+  })
+}
+
+const serachProductQuestion = () => {
+  rl.question('Digite o nome do produto que está procurando: ', (nameProduct) => {
+    seachProduct(products, nameProduct)
     })
-    
 }
 
-rl.question('Qual seu nome? ', (resposta) => {
-  perguntarAlgo();
-});
+const addWalletQuestion = (newProduct) => {
+  rl.question('Deseja adicionar o produto e quantidade a sua sacola? Digite sim ou não: ', (answer) => {
+    if(answer === 'sim' || answer === 'Sim'){
+      addWallet(newProduct)
+    } else if(answer.toUpperCase() === 'NÃO' || answer.toUpperCase() === 'NAO'){
+      serachProductQuestion()
+    } else {
+      init()
+    }
+  })
+}
+
+const init = () => {
+    rl.question('O que deseja fazer? Digte: sacola, procurar ou sair? ', (answer) => {
+      if(answer === 'procurar' || answer === 'Procurar'){
+        serachProductQuestion()
+      } else if(answer === 'sair' || answer === 'Sair') {
+        rl.close();
+      } else if(answer === 'sacola' || answer === 'Sacola') {
+        listWallet()
+      } else {
+        console.log(`${chalk.red("Comando inválido, tente novamete!")} \n`)
+        init()
+      }
+    })
+}
+
+init()
