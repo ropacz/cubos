a0f8d5583de81d1b9de70f4749e76dbc70cd0fa9
Rodrigo Paczkovski <paczweb@gmail.com>
2020-08-20 21:19:55 -0300

    documentação: criação de um gerenciador de tarefas

---
+++ front-end/aula-16/todo/script.js
@@ -0,0 +1,53 @@
+
+const form = document.querySelector('form')
+const input = document.querySelector('form input')
+const button = document.querySelector('form button')
+const toDo = document.querySelector('.todo')
+const done = document.querySelector('.done')
+
+
+form.addEventListener("submit", event => {
+    event.preventDefault()
+
+    const task = document.createElement("li")
+    
+    const checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+
+    checkbox.addEventListener("input", doneTask)
+
+    const text = document.createElement("span")
+    text.innerHTML = input.value
+
+    const buttonDelete = document.createElement("button")
+    buttonDelete.innerHTML = "Deletar"
+
+    buttonDelete.addEventListener("click", deleteTask)
+
+    task.append(checkbox)
+    task.append(text)
+    task.append(buttonDelete)
+
+    toDo.append(task)
+    
+    input.value = ""
+})
+
+function deleteTask(event) {
+    const deleteButton = event.target
+    const liTask = deleteButton.closest("li")
+
+    liTask.remove()
+}
+
+function doneTask(event) {
+    const checkbox = event.target
+    const liTask = checkbox.closest("li")
+
+    if(checkbox.checked) {
+        done.append(liTask)
+    } else {
+        todo.append(liTask)
+    }
+
+}
