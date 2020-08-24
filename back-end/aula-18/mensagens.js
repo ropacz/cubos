d23b8cfb1189adf10a17771287716a64faba4be8
Rodrigo Paczkovski <paczweb@gmail.com>
2020-08-24 16:09:16 -0300

    funcionalidade: adição dos endereços as mensagens

--- back-end/aula-18/mensagens.js
+++ back-end/aula-18/mensagens.js
@@ -1,35 +1,57 @@
 const fs = require('fs')
 
+let mensagens = []
+let enderecos = []
 
+// leitura das mensagens de forma assincrona
 fs.readFile('cartas.txt', (err, data) => {
     if (err) {
         return console.log(err);
     }
-
-    let mensagens = []
     mensagens = data.toString().split('---')
 
-    // passar mensagens para função
-    converterTexto(mensagens)
+    // quando termina de carregar as mensagens são chamas as próximas funções
+    carregaEnderecos()
+    formatarTexto()
 })
 
-const converterTexto = (mensagens) => {
+// leitura dos endereços de forma sincrona
+const carregaEnderecos = () => {
+    enderecos = fs.readFileSync('enderecos.txt').toString().split('---')
+}
+
+// fução para formatar o texto
+const formatarTexto = () => {
     let msg = []
+    let end = []
     let msgFormatada = ''
 
+    // percorrer todas as mensagens
     for (let i = 0; i < mensagens.length; i++) {
         msg = mensagens[i].trim().split('\n')
 
+        // inicio da mensagem
         msgFormatada += "[INICIO DA MENSAGEM] \n"
         msgFormatada += `Remetente: ${msg[0]} \n`
         msgFormatada += `Destinatário: ${msg[1]} \n`
-        msgFormatada += `Mensagem: ${msg[2]} \n`
 
+        // percorrer todos os endereços
+        for(let j = 0; j < enderecos.length; j++){
+            end[j] = enderecos[j].trim().split('\n')
+            
+            // caso haja um nome igual ao destinatário adiciona o endereço na mensagem
+            if(msg[1] === end[j][0]) {
+                msgFormatada += `Endereço: ${end[j][1]} \n`
+            }
+        }
+        
+        // mensagem e texto final
+        msgFormatada += `Mensagem: ${msg[2]} \n`
         msgFormatada += "[FIM DA MENSAGEM] \n"
 
     }
-    console.log(msgFormatada)
-
+    
+    // criar um arquivo de texto com as mensagens
     fs.writeFile("mensagens.txt", msgFormatada, (erro) => {
         if (erro) {
             throw erro;
@@ -38,6 +60,5 @@ const converterTexto = (mensagens) => {
     });
 
 }
-
 
 
