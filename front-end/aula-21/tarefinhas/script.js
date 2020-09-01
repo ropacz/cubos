e33c478ea1d1711cef3a3c64ea8238afc16c3295
Rodrigo Paczkovski <paczweb@gmail.com>
2020-09-01 13:34:20 -0300

    documentação: envio do exercício lista de tarefas 2.0

--- front-end/aula-21/tarefinhas/script.js
+++ front-end/aula-21/tarefinhas/script.js
@@ -4,14 +4,19 @@ const tarefas = document.querySelector('.tarefas ul')
 const campoTarefa = document.querySelector('#campo')
 const totalDeItens = document.querySelector('.total span')
 const botaoTodasTarefas = document.querySelector('.filtros .todas')
+
+const botoesInfos = document.querySelectorAll('.filtros button')
+
 const botaoAFazer = document.querySelector('.filtros .aFazer')
 const botaoFeitas = document.querySelector('.filtros .feitas')
 const botaoLimpar = document.querySelector('.limpar button')
 
+let ativo = null
+
 let listaDeTarefas = []
 
 const total = () => {
-    totalDeItens.innerText = listaDeTarefas.filter((item) => item.feito === true).length
+    totalDeItens.innerText = listaDeTarefas.filter((item) => item.feito === false).length
 }
 
 
@@ -22,6 +27,8 @@ const renderizarTarefas = (filtro = null) => {
     //     <input type="checkbox" onclick="marcarComoFeito(${index})" ${feito ? "checked" : ""}>
     //     <span> ${texto} </span> <a href="#" onclick="deletar(${index})">Deletar</a></li>`
     // }).join('')
+
+    ativo = filtro
 
     let array = listaDeTarefas
 
@@ -36,7 +43,7 @@ const renderizarTarefas = (filtro = null) => {
     array.forEach(({ texto, feito }, index) => {
 
         let li = document.createElement('li')
-        li.classList.add = (feito) ? 'feito' : ''
+        
 
         let checkbox = document.createElement('input')
         checkbox.setAttribute("type", "checkbox")
@@ -44,7 +51,9 @@ const renderizarTarefas = (filtro = null) => {
         checkbox.checked = (feito) ? true : false
 
         let tarefaTexto = document.createElement('span')
+        tarefaTexto.classList.toggle("feito", feito)
         tarefaTexto.innerText = texto
+
 
         let deletar = document.createElement('a')
         deletar.setAttribute("href", "#")
@@ -77,16 +86,31 @@ checkTudoFeito.addEventListener('change', (event) => {
     marcarTodasTarefas(event.target.checked)
 })
 
+const removerClasseAtivo = () => {
+    for(let i = 0; i < botoesInfos.length; i++){
+        if(botoesInfos[i].classList.contains('ativo')) {
+            botoesInfos[i].classList.remove('ativo')
+        }
+    }
+}
+
+
 botaoTodasTarefas.addEventListener("click", () => {
     renderizarTarefas()
+    removerClasseAtivo()
+    botaoTodasTarefas.classList.add('ativo')
 })
 
 botaoAFazer.addEventListener("click", () => {
     renderizarTarefas(false)
+    removerClasseAtivo()
+    botaoAFazer.classList.add('ativo')
 })
 
 botaoFeitas.addEventListener("click", () => {
     renderizarTarefas(true)
+    removerClasseAtivo()
+    botaoFeitas.classList.add('ativo')
 })
 
 botaoLimpar.addEventListener("click", () => {
@@ -102,18 +126,19 @@ const adicionarTarefa = (texto) => {
         }
     )
     campoTarefa.value = ''
-    renderizarTarefas()
+    console.log(ativo)
+    renderizarTarefas(ativo)
 }
 
 
 const deletar = (pos) => {
     listaDeTarefas.splice(pos, 1)
-    renderizarTarefas()
+    renderizarTarefas(ativo)
 }
 
 const marcarComoFeito = (pos) => {
     listaDeTarefas[pos].feito = !listaDeTarefas[pos].feito
-    renderizarTarefas()
+    renderizarTarefas(ativo)
 }
 
 const marcarTodasTarefas = (todas) => {
